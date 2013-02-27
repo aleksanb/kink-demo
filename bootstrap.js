fb4641e0f2f182d711253a3ab16f3bc769310e6a
Stian Jensen <me@stianj.com>
2013-02-28 00:28:29 +0100

    New map. FINAL MAP

--- bootstrap.js
+++ bootstrap.js
@@ -79,7 +79,7 @@ function bootstrap(){
     twoDCanvas.style.background = "transparent";
     tdx = twoDCanvas.getContext("2d");
     resize();
-    Math.seedrandom('kinkisawesomeyo');    
+    Math.seedrandom('kinkisawesomebitchyo');    
     document.body.appendChild(renderer.domElement);
     document.body.appendChild(twoDCanvas);
     var file_format = Modernizr.audio.ogg ? 'ogg' : 'mp3';
