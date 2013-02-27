1227eb89dd1b8ba3a7285b0d2390ce8c41a30654
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-02-27 03:03:35 +0100

    apple has no stalk now, awesome texture instead for main body

--- Apple.js
+++ Apple.js
@@ -3,21 +3,15 @@ function Apple( scene, vector, radius ) {
 	this.radius = radius;
 
 	var appleGeometry = new THREE.SphereGeometry( radius, 20, 20 );
-	var appleStilkGeometry = new THREE.SphereGeometry( radius / 2, 20, 20 );
 	this.appleMesh = new THREE.Mesh( appleGeometry, materials.appleBody );
-	this.appleStilkMesh = new THREE.Mesh( appleStilkGeometry, materials.appleStilk );
 
 	this.appleMesh.position.copy( vector );
-	this.appleStilkMesh.position.addVectors( vector, new THREE.Vector3( 0, radius * 3/5, 0 ) );
-
 	scene.add( this.appleMesh );
-	scene.add( this.appleStilkMesh );
 
 }
 
 Apple.prototype.visibleToggle = function() {
 	this.appleMesh.visible = ( this.appleMesh.visible )? false : true;
-	this.appleStilkMesh.visible = ( this.appleStilkMesh.visible )? false : true;
 }
 
 Apple.prototype.update = function() {
@@ -25,7 +19,6 @@ Apple.prototype.update = function() {
 
 	if ( Math.abs( distance ) < this.radius ) {
 		this.appleMesh.visible = false;
-		this.appleStilkMesh.visible = false;
 
 		currentApple += 1;
         if ( currentApple < apples.length ) {
